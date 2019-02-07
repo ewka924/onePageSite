1c292878b5a8115788f169b92c0ea530a5d4e757
ewka924 <ewka924@gmail.com>
2019-02-07 19:26:26 +0100

    change round counter

--- project/js/js.js
+++ project/js/js.js
@@ -25,83 +25,17 @@ toggleMenuButton.addEventListener('click', toggleNav);
 
 
 
-let ctx;
-let centerX;
-let radius;
-let width;
-let height;
-let all = 0;
-let data;
+let circleBar = document.querySelectorAll(".circleBar");
 
 
- function degreesToRadians(deg) {
-
- 			return (deg/180) * Math.PI;
- 
-        
-      }
-
-function percentToRadians(percentage){
-	  
-
-  		
-  		console.log("data " + data);	 
-  	
-  			console.log("Percentage " + percentage);
-  			var degrees = percentage * 360 / 100;
-  			return degreesToRadians(degrees + 270);
-  		
-        
-        
-}
-
-
-
-
-function drawCircle(){
-	canvas.forEach(canvas =>{
-
-	ctx = canvas.getContext('2d');
-
-	width = canvas.width = 120;
-	heigth = canvas.height = 120;
-
-	centerX = canvas.width/2;
-	centerY = canvas.height/2;
-	radius = 50;
-	data = canvas.dataset.percentage;
-	let startAngle = (0/180)*Math.PI;
-	let endAngle = percentToRadians(all);
-	ctx.lineWidth = 2;
-	ctx.fillStyle = '#7b6c63';
-	ctx.strokeStyle = '#7b6c63';
-	ctx.beginPath();
-	ctx.arc(centerX, centerY, radius, startAngle, endAngle, false);
-	ctx.stroke();
-	all ++;
-
+circleBar.forEach(el =>{
+	let lenght = el.getTotalLength();
+	let actualPercent = el.dataset.procent;
+	console.log(actualPercent);
+	let percent = (actualPercent*lenght)/100;
+	console.log(`"${percent}"`);
+	el.style.strokeDashoffset= percent;
+	
 });
-	}
-
-//interval fucntion
-function interval(){
-	setInterval(drawCircle, 1000);
-}
-
-//get canvas from html
-const canvas = document.querySelectorAll(".counterCircle");
-console.log(canvas);
-
-//set interval function to all canvas
-canvas.forEach(canvas =>{
-interval();
-});
-
-
-
-
-
-
-
 
 
